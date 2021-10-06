679e9cf1bc348e9b5a3e49619c257b5c2e4bac89
Viktor Bujko <viktorbjk@gmail.com>
2021-10-06 23:35:55 +0200

    Submit unicafe step2 (exercise 1.7)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -27,7 +27,6 @@ const App = () => {
     console.log('good value before setting', good)
     setGood(good + 1) 
   }
-
   const setNeutralFeedback = () => { 
     console.log('neutral value before setting', neutral)
     setNeutral(neutral + 1) 
@@ -37,10 +36,38 @@ const App = () => {
     setBad(bad + 1) 
   }
 
+  const weightedAvg = ({ values, weights }) => {
+
+    let sum = 0
+
+    if (values.length === weights.length) {
+      let scores = values.map((_, i) => values[i] * weights[i])
+      scores.forEach(value => sum += value)
+    
+      return all === 0 
+                ? 0 
+                : sum / all
+
+    } else {
+      console.log('Lenghts of arrays are not equal.')
+      return Number.NaN
+    }
+  }
+
   const goodLabel = 'Good'
   const neutralLabel = 'Neutral'
   const badLabel = 'Bad'
+  const allLabel = 'All'
+  const avgLabel = 'Average'
+  const positiveLabel = 'Positive'
 
+  const all = good + neutral + bad
+  const positivePercent = all === 0 ? 0 : (good * 100) / all + ' %'
+  const avg = weightedAvg({
+    values: [good, neutral, bad],
+    weights: [1, 0, -1]
+  })
+  
   return (
     <div>
       <h1>Give feedback</h1>
@@ -51,6 +78,9 @@ const App = () => {
       <Stats label={goodLabel} value={good}/>
       <Stats label={neutralLabel} value={neutral}/>
       <Stats label={badLabel} value={bad}/>
+      <Stats label={allLabel} value={all}/>
+      <Stats label={avgLabel} value={avg}/>
+      <Stats label={positiveLabel} value={positivePercent}/>
     </div>
   )
 }
